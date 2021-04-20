e9814adeefcc82c5670356e70a15f733ce569d4d
CoderTeam2 <72189661+CoderTeam2@users.noreply.github.com>
2021-04-20 16:51:19 +0530

    Create script.js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-//ok aman
-
+var player = "x";
+function draw (id){
+  if (player = "x"){
+    document.getElementById().style.backgroundImage = "url("")"
+    player = "y"
+  }else {
+    document.getElementById().style.backgroundImage = "url("")"
+    player = "x"
+  }
+}
+if () {
+    
+}

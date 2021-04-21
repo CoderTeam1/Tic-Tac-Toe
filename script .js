05fbd74349042a10fd290e4a9547a7ebd0ef48d5
CoderTeam1 <72059049+CoderTeam1@users.noreply.github.com>
2021-04-21 15:41:55 +0530

    Update script .js

--- script .js
+++ script .js
@@ -1,36 +1,36 @@
 function cell1() {
-  document.querySelector("1").innerHTML = "X";
+  document.querySelector("word1").innerHTML = "X";
 }
 
 function cell2() {
-  document.querySelector("2").innerHTML = "X";
+  document.querySelector("word2").innerHTML = "X";
 }
 
 function cell3() {
-  document.querySelector("3").innerHTML = "X";
+  document.querySelector("word3").innerHTML = "X";
 }
 
 function cell4() {
-  document.querySelector("4").innerHTML = "X";
+  document.querySelector("word4").innerHTML = "X";
 }
 
 function cell5() {
-  document.querySelector("5").innerHTML = "X";
+  document.querySelector("word5").innerHTML = "X";
 }
 
 function cell6() {
-  document.querySelector("6").innerHTML = "X";
+  document.querySelector("word6").innerHTML = "X";
 }
 
 function cell7() {
-  document.querySelector("7").innerHTML = "X";
+  document.querySelector("word7").innerHTML = "X";
 }
 
 function cell8() {
-  document.querySelector("8").innerHTML = "X";
+  document.querySelector("word8").innerHTML = "X";
 }
 
 
 function cell9() {
-  document.querySelector("9").innerHTML = "X";
+  document.querySelector("word9").innerHTML = "X";
 }

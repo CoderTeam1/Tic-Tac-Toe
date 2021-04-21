a9a5072ec8c67b5c293c34086501b70f799a572d
CoderTeam2 <72189661+CoderTeam2@users.noreply.github.com>
2021-04-21 15:44:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,35 +10,35 @@ function draw (id){
   }
 }
 if (document.getElementById("cell1").innerHTML == document.getElementById("cell5").innerHTML && document.getElementById("cell1").innerHTML == document.getElementById("cell9").innerHTML) {
-   winner = document.getElementById().innerHTML;
-   document.getElementById().innerHTML = winner.concat(" wins!!!");
+   winner = document.getElementById("cell1").innerHTML;
+   document.getElementById("cell1").innerHTML = winner.concat(" wins!!!");
    document.getElementById("#grid").visibility = "hidden";
 }else if(document.getElementById("cell3").innerHTML == document.getElementById("cell5").innerHTML && document.getElementById("cell3").innerHTML == document.getElementById("cell7").innerHTML) {
-   winner = document.getElementById().innerHTML;
-   document.getElementById().innerHTML = winner.concat(" wins!!!");
+   winner = document.getElementById("cell3").innerHTML;
+   document.getElementById("cell3").innerHTML = winner.concat(" wins!!!");
    document.getElementById("#grid").visibility = "hidden";
 }else if(document.getElementById("cell1").innerHTML == document.getElementById("cell4").innerHTML && document.getElementById("cell1").innerHTML == document.getElementById("cell7").innerHTML) {
-   winner = document.getElementById().innerHTML;
-   document.getElementById().innerHTML = winner.concat(" wins!!!");
+   winner = document.getElementById("cell1").innerHTML;
+   document.getElementById("cell1").innerHTML = winner.concat(" wins!!!");
    document.getElementById("#grid").visibility = "hidden";         
 }else if(document.getElementById("cell3").innerHTML == document.getElementById("cell6").innerHTML && document.getElementById("cell3").innerHTML == document.getElementById("cell9").innerHTML){
-   winner = document.getElementById().innerHTML;
-   document.getElementById().innerHTML = winner.concat(" wins!!!");
+   winner = document.getElementById("cell3").innerHTML;
+   document.getElementById("cell3").innerHTML = winner.concat(" wins!!!");
    document.getElementById("#grid").visibility = "hidden";
 }else if(document.getElementById("cell2").innerHTML == document.getElementById("cell5").innerHTML && document.getElementById("cell2").innerHTML == document.getElementById("cell8").innerHTML){
-   winner = document.getElementById().innerHTML;
-   document.getElementById().innerHTML = winner.concat(" wins!!!");
+   winner = document.getElementById("cell2").innerHTML;
+   document.getElementById("cell2").innerHTML = winner.concat(" wins!!!");
    document.getElementById("#grid").visibility = "hidden";
 }else if(document.getElementById("cell1").innerHTML == document.getElementById("cell2").innerHTML && document.getElementById("cell1").innerHTML == document.getElementById("cell3").innerHTML){
-   winner = document.getElementById().innerHTML;
-   document.getElementById().innerHTML = winner.concat(" wins!!!");
+   winner = document.getElementById("cell1").innerHTML;
+   document.getElementById("cell1").innerHTML = winner.concat(" wins!!!");
    document.getElementById("#grid").visibility = "hidden";
 }else if(document.getElementById("cell4").innerHTML == document.getElementById("cell5").innerHTML && document.getElementById("cell4").innerHTML == document.getElementById("cell6").innerHTML){
-   winner = document.getElementById().innerHTML;
-   document.getElementById().innerHTML = winner.concat(" wins!!!");
+   winner = document.getElementById("cell4").innerHTML;
+   document.getElementById("cell4").innerHTML = winner.concat(" wins!!!");
    document.getElementById("#grid").visibility = "hidden";
 }else if(document.getElementById("cell7").innerHTML == document.getElementById("cell8").innerHTML && document.getElementById("cell7").innerHTML == document.getElementById("cell9").innerHTML){
-   winner = document.getElementById().innerHTML;
-   document.getElementById().innerHTML = winner.concat(" wins!!!");
+   winner = document.getElementById("cell7").innerHTML;
+   document.getElementById("cell7").innerHTML = winner.concat(" wins!!!");
    document.getElementById("#grid").visibility = "hidden";
 }

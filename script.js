97c5110affa65334b8efe771c0f07dcce1b90bd9
CoderTeam2 <72189661+CoderTeam2@users.noreply.github.com>
2021-04-21 14:49:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var player = "x";
 function draw (id){
   if (player = "x"){
-    document.getElementById().style.backgroundImage = "url("")"
+    document.getElementById().style.backgroundImage = "url("https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png")"
     player = "y"
   }else {
     document.getElementById().style.backgroundImage = "url("")"

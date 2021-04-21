110d9ded09745747eceef1a434f47f6fe54763a7
CoderTeam1 <72059049+CoderTeam1@users.noreply.github.com>
2021-04-21 15:51:10 +0530

    Update script .js

--- script .js
+++ script .js
@@ -1,36 +1,35 @@
 function cell1() {
-  document.querySelector("word1").innerHTML = "X";
+  document.getElementById("word1").innerHTML = "X";
 }
 
 function cell2() {
-  document.querySelector("word2").innerHTML = "X";
+  document.getElementById("word2").innerHTML = "X";
 }
 
 function cell3() {
-  document.querySelector("word3").innerHTML = "X";
+  document.getElementById("word3").innerHTML = "X";
 }
 
 function cell4() {
-  document.querySelector("word4").innerHTML = "X";
+  document.getElementById("word4").innerHTML = "X";
 }
 
 function cell5() {
-  document.querySelector("word5").innerHTML = "X";
+  document.getElementById("word5").innerHTML = "X";
 }
 
 function cell6() {
-  document.querySelector("word6").innerHTML = "X";
+  document.getElementById("word6").innerHTML = "X";
 }
 
 function cell7() {
-  document.querySelector("word7").innerHTML = "X";
+  document.getElementById("word7").innerHTML = "X";
 }
 
 function cell8() {
-  document.querySelector("word8").innerHTML = "X";
+  document.getElementById("word8").innerHTML = "X";
 }
 
-
 function cell9() {
-  document.querySelector("word9").innerHTML = "X";
+  document.getElementById("word9").innerHTML = "X";
 }

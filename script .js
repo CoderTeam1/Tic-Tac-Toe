f5a1089199381269a8654ae510b4fd43a3d4c568
CoderTeam1 <72059049+CoderTeam1@users.noreply.github.com>
2021-04-21 15:41:52 +0530

    Update script .js

--- script .js
+++ script .js
@@ -1,36 +1,36 @@
 function cell1() {
-  document.querySelector("cell1").innerHTML = "X";
+  document.querySelector("1").innerHTML = "X";
 }
 
 function cell2() {
-  document.querySelector("cell2").innerHTML = "X";
+  document.querySelector("2").innerHTML = "X";
 }
 
 function cell3() {
-  document.querySelector("cell3").innerHTML = "X";
+  document.querySelector("3").innerHTML = "X";
 }
 
 function cell4() {
-  document.querySelector("cell4").innerHTML = "X";
+  document.querySelector("4").innerHTML = "X";
 }
 
 function cell5() {
-  document.querySelector("cell5").innerHTML = "X";
+  document.querySelector("5").innerHTML = "X";
 }
 
 function cell6() {
-  document.querySelector("cell6").innerHTML = "X";
+  document.querySelector("6").innerHTML = "X";
 }
 
 function cell7() {
-  document.querySelector("cell7").innerHTML = "X";
+  document.querySelector("7").innerHTML = "X";
 }
 
 function cell8() {
-  document.querySelector("cell8").innerHTML = "X";
+  document.querySelector("8").innerHTML = "X";
 }
 
 
 function cell9() {
-  document.querySelector("cell9").innerHTML = "X";
+  document.querySelector("9").innerHTML = "X";
 }

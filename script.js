080b4934eae2a937625ce1e1de6fe835f3fe504c
CoderTeam2 <72189661+CoderTeam2@users.noreply.github.com>
2021-04-21 14:57:39 +0530

    Create script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var player = "x";
 function draw (id){
   if (player = "x"){
-    document.getElementById().style.backgroundImage = "url("https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png")
+    document.getElementById(id).style.backgroundImage = url("https://www.clipartmax.com/png/middle/134-1340885_swarm-bee-x-delete-comments-tic-tac-toe-cross.png")
     player = "y"
   }else {
-    document.getElementById().style.backgroundImage = "url("https://www.clipartmax.com/png/small/440-4408148_transparent-o-tic-tac-toe-o.png")
+    document.getElementById(id).style.backgroundImage = url("https://www.clipartmax.com/png/small/440-4408148_transparent-o-tic-tac-toe-o.png")
     player = "x"
   }
 }
 if () {
-    
+   
 }else if() {
 
 }else if() {

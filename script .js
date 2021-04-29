71c2f56a6de451b45363170a256331acb3f8424f
CoderTeam1 <72059049+CoderTeam1@users.noreply.github.com>
2021-04-29 15:07:33 +0530

    Update script .js

--- script .js
+++ script .js
@@ -1,35 +1,35 @@
-function cell1() {
+function 1() {
   document.getElementById("word1").innerHTML = "X";
 }
 
-function cell2() {
+function 2() {
   document.getElementById("word2").innerHTML = "X";
 }
 
-function cell3() {
+function 3() {
   document.getElementById("word3").innerHTML = "X";
 }
 
-function cell4() {
+function 4() {
   document.getElementById("word4").innerHTML = "X";
 }
 
-function cell5() {
+function 5() {
   document.getElementById("word5").innerHTML = "X";
 }
 
-function cell6() {
+function 6() {
   document.getElementById("word6").innerHTML = "X";
 }
 
-function cell7() {
+function 7() {
   document.getElementById("word7").innerHTML = "X";
 }
 
-function cell8() {
+function 8() {
   document.getElementById("word8").innerHTML = "X";
 }
 
-function cell9() {
+function 9() {
   document.getElementById("word9").innerHTML = "X";
 }

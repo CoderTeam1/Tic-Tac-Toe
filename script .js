910aa0ff3b1a0bbbe3f7abd214d26fe61aad61e5
Ninjaxxd28 <76779878+Ninjaxxd28@users.noreply.github.com>
2021-09-18 12:53:29 +0530

    Update script .js

--- script .js
+++ script .js
@@ -33,3 +33,39 @@ function word8() {
 function word9() {
   document.getElementById("word9").innerHTML = "X";
 }
+
+function word1()
+document.getElementById("word1").innerHTML = "O";
+}
+
+function word2() {
+  document.getElementById("word2").innerHTML = "O";
+}
+
+function word3() {
+  document.getElementById("word3").innerHTML = "O";
+}
+
+function word4() {
+  document.getElementById("word4").innerHTML = "O";
+}
+
+function word5() {
+  document.getElementById("word5").innerHTML = "O";
+}
+
+function word6() {
+  document.getElementById("word6").innerHTML = "O";
+}
+
+function word7() {
+  document.getElementById("word7").innerHTML = "O";
+}
+
+function word8() {
+  document.getElementById("word8").innerHTML = "O";
+}
+
+function word9() {
+  document.getElementById("word9").innerHTML = "O";
+}
